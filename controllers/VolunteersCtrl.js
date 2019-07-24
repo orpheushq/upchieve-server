c03b4fd344e5f6d867501795f0cb2aefa6cff7d2
Georgia Gallant <georgia_gallant@brown.edu>
2019-07-24 16:57:51 -0400

    Removed global variables on server side

--- controllers/VolunteersCtrl.js
+++ controllers/VolunteersCtrl.js
@@ -1,61 +1,60 @@
 var User = require('../models/User')
 
-var daysOfWeek // column headers
-var timesOfDay // row headers
-var aggAvailabilities = {}
-
 /**
  * Helper function that, given a single users's
  * availability, adds when they are free to the
  * aggAvailabilities object
  * @param {*} availability
  */
-function aggregateAvailabilities (availability) {
+function aggregateAvailabilities (availability, aggAvailabilities) {
   for (const day in availability) {
     for (const time in availability[day]) {
       // '$init' property and others are not skipped when nested
       if (time !== '$init' && availability[day].hasOwnProperty([time])) {
         // create headers based on the user's availability object
-        if (!daysOfWeek) {
-          daysOfWeek = Object.keys(availability)
-          daysOfWeek.shift() // gets rid of $init enum param
+        if (!aggAvailabilities.daysOfWeek) {
+          aggAvailabilities.daysOfWeek = Object.keys(availability)
+          aggAvailabilities.daysOfWeek.shift() // gets rid of $init enum param
         }
-        if (!timesOfDay) {
-          timesOfDay = Object.keys(availability[day])
-          timesOfDay.shift() // gets rid of $init enum param
+        if (!aggAvailabilities.timesOfDay) {
+          aggAvailabilities.timesOfDay = Object.keys(availability[day])
+          aggAvailabilities.timesOfDay.shift() // gets rid of $init enum param
         }
         // adds 1 to leave room for headers
-        let dayIndex = daysOfWeek.indexOf(day) + 1
-        let timeIndex = timesOfDay.indexOf(time) + 1
+        let dayIndex = aggAvailabilities.daysOfWeek.indexOf(day) + 1
+        let timeIndex = aggAvailabilities.timesOfDay.indexOf(time) + 1
 
         if (availability[day][time]) {
           aggAvailabilities.table[dayIndex][timeIndex]++
         }
-
-        let dayTimeCell = aggAvailabilities.table[dayIndex][timeIndex]
-
-        // tracks the min and max # of volunteers who signed up for any time this week
-        if (aggAvailabilities.min === null || (dayTimeCell < aggAvailabilities.min)) {
-          aggAvailabilities.min = dayTimeCell
-        }
-        if (dayTimeCell > aggAvailabilities.max) {
-          aggAvailabilities.max = dayTimeCell
-        }
       }
     }
   }
+  return aggAvailabilities
+}
+
+function findMinAndMax (aggAvailabilities) {
+  let flatTable = aggAvailabilities.table.flat()
+  var filtered = flatTable.filter(function (item) {
+    return !isNaN(item)
+  })
+  filtered.shift() // gets rid of top left empty cell
+  aggAvailabilities.min = Math.min.apply(Math, filtered)
+  aggAvailabilities.max = Math.max.apply(Math, filtered)
+  return aggAvailabilities
 }
 
 /**
  * Helper function to add headers to the table once table is created
  */
-function addHeaders () {
-  daysOfWeek.forEach(function (value, index) {
-    aggAvailabilities.table[index + 1][0] = daysOfWeek[index]
-  })
-  timesOfDay.forEach(function (value, index) {
-    aggAvailabilities.table[0][index + 1] = timesOfDay[index]
-  })
+function addHeaders (aggAvailabilities) {
+  for (var dayIndex = 0; dayIndex < aggAvailabilities.daysOfWeek.length; dayIndex++) {
+    aggAvailabilities.table[dayIndex + 1][0] = aggAvailabilities.daysOfWeek[dayIndex]
+  }
+  for (var timeIndex = 0; timeIndex < aggAvailabilities.timesOfDay.length; timeIndex++) {
+    aggAvailabilities.table[0][timeIndex + 1] = aggAvailabilities.timesOfDay[timeIndex]
+  }
+  return aggAvailabilities
 }
 
 module.exports = {
@@ -69,8 +68,8 @@ module.exports = {
   getVolunteersAvailability: function (options, callback) {
     var certifiedSubjectQuery = options.certifiedSubject + '.passed'
     User.find({ isVolunteer: true, hasSchedule: true, [certifiedSubjectQuery]: true }, function (err, users) {
-
       // defining and resetting variables
+      var aggAvailabilities = {}
       aggAvailabilities.table = Array(8).fill(0).map(() => Array(25).fill(0))
       aggAvailabilities.min = null
       aggAvailabilities.max = 0
@@ -80,9 +79,12 @@ module.exports = {
         return callback(null, err)
       } else {
         users.forEach(function (user) {
-          if (user.availability) aggregateAvailabilities(user.availability)
+          if (user.availability) {
+            aggAvailabilities = aggregateAvailabilities(user.availability, aggAvailabilities)
+          }
         })
-        addHeaders()
+        aggAvailabilities = addHeaders(aggAvailabilities)
+        aggAvailabilities = findMinAndMax(aggAvailabilities)
         return callback(aggAvailabilities, null)
       }
     })
