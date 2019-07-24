ef4448123eac2a6ae4ef0abc02055ade1e3e238b
Georgia Gallant <georgia_gallant@brown.edu>
2019-07-24 11:07:30 -0400

    Added comments for clarity

--- controllers/VolunteersCtrl.js
+++ controllers/VolunteersCtrl.js
@@ -1,14 +1,21 @@
 var User = require('../models/User')
 
-var daysOfWeek
-var timesOfDay
+var daysOfWeek // column headers
+var timesOfDay // row headers
 var aggAvailabilities = {}
 
+/**
+ * Helper function that, given a single users's
+ * availability, adds when they are free to the
+ * aggAvailabilities object
+ * @param {*} availability
+ */
 function aggregateAvailabilities (availability) {
   for (const day in availability) {
     for (const time in availability[day]) {
       // '$init' property and others are not skipped when nested
       if (time !== '$init' && availability[day].hasOwnProperty([time])) {
+        // create headers based on the user's availability object
         if (!daysOfWeek) {
           daysOfWeek = Object.keys(availability)
           daysOfWeek.shift() // gets rid of $init enum param
@@ -17,13 +24,17 @@ function aggregateAvailabilities (availability) {
           timesOfDay = Object.keys(availability[day])
           timesOfDay.shift() // gets rid of $init enum param
         }
+        // adds 1 to leave room for headers
         let dayIndex = daysOfWeek.indexOf(day) + 1
         let timeIndex = timesOfDay.indexOf(time) + 1
+
         if (availability[day][time]) {
           aggAvailabilities.table[dayIndex][timeIndex]++
         }
 
         let dayTimeCell = aggAvailabilities.table[dayIndex][timeIndex]
+
+        // tracks the min and max # of volunteers who signed up for any time this week
         if (aggAvailabilities.min === null || (dayTimeCell < aggAvailabilities.min)) {
           aggAvailabilities.min = dayTimeCell
         }
@@ -35,6 +46,9 @@ function aggregateAvailabilities (availability) {
   }
 }
 
+/**
+ * Helper function to add headers to the table once table is created
+ */
 function addHeaders () {
   daysOfWeek.forEach(function (value, index) {
     aggAvailabilities.table[index + 1][0] = daysOfWeek[index]
@@ -45,15 +59,23 @@ function addHeaders () {
 }
 
 module.exports = {
+  /**
+   * Gets all users who are volunteers, and who are certified in the
+   * subject passed in, and aggregates their availability tables into
+   * aggAvailabilities.table
+   * @param {*} options
+   * @param {*} callback
+   */
   getVolunteersAvailability: function (options, callback) {
-    // defining and resetting variables
-    aggAvailabilities.table = Array(8).fill(0).map(() => Array(25).fill(0))
-    aggAvailabilities.min = null
-    aggAvailabilities.max = 0
-    aggAvailabilities.table[0][0] = ''
-
     var certifiedSubjectQuery = options.certifiedSubject + '.passed'
     User.find({ isVolunteer: true, hasSchedule: true, [certifiedSubjectQuery]: true }, function (err, users) {
+
+      // defining and resetting variables
+      aggAvailabilities.table = Array(8).fill(0).map(() => Array(25).fill(0))
+      aggAvailabilities.min = null
+      aggAvailabilities.max = 0
+      aggAvailabilities.table[0][0] = ''
+
       if (err) {
         return callback(null, err)
       } else {
@@ -66,6 +88,10 @@ module.exports = {
     })
   },
 
+  /**
+   * Gets all users who are volunteers
+   * @param {*} callback
+   */
   getVolunteers: function (callback) {
     User.find({ isVolunteer: true }, function (err, users) {
       if (err) {
