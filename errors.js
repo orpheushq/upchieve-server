345105debff5064d2707ed8cca84515410eb00c6
David Nisson <d.m.nisson@gmail.com>
2019-09-02 10:59:57 -0700

    clarify comment

--- errors.js
+++ errors.js
@@ -68,7 +68,7 @@ module.exports = {
     return err
   },
   
-  // augment error with needed information
+  // augment exiting error object with needed information for proper reporting
   augmentError: function (err) {
     err.statusCode = this.statusFor(err)
     err._uid = uid()
