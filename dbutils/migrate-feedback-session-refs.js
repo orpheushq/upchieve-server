1ab6c37e72b94ad2657e721e68e24e32bdf5825c
David Nisson <d.m.nisson@gmail.com>
2019-12-24 09:26:18 -0800

    eslint

--- dbutils/migrate-feedback-session-refs.js
+++ dbutils/migrate-feedback-session-refs.js
@@ -3,25 +3,25 @@ const dbconnect = require('./dbconnect')
 
 const Feedback = require('../models/Feedback')
 
-dbconnect(mongoose, function() {
+dbconnect(mongoose, function () {
   Feedback.find({
     session: { $exists: false }
   }).then(feedbacks => {
     const feedbackUpdatePromises = feedbacks.map(f => {
       const fRawObj = f.toObject()
-      
+
       console.log(`Migrating feedback submission ${f._id}`)
-      
+
       // add "session" property
       f.set('session', fRawObj.sessionId ? mongoose.Types.ObjectId(fRawObj.sessionId) : undefined)
-      
+
       // remove old "sessionId" property
       f.set('sessionId', undefined, { strict: false })
-      
+
       // save
       return f.save()
     })
-    
+
     return Promise.all(feedbackUpdatePromises)
   }).catch(err => {
     console.log(err)
